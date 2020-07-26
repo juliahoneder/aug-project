57dabdb104df702f0e688e7265d717e13ce7e343
juliahoneder <50960655+juliahoneder@users.noreply.github.com>
2020-07-26 19:53:59 +0200

    add/remove value script

---
+++ AUG-Project/Assets/Scripts/Custom/MegaCreditDown.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Vuforia;
+
+public class MegaCreditDown : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public int initialCredit = 20;
+    public int productionCredit = 1;
+    private int currentCredit = 20;
+    private int stepSize = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject virtualButtonMegaCreditDown = GameObject.Find("MegaCreditDown");
+        virtualButtonMegaCreditDown.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        UnityEngine.Debug.Log("Button pressed");
+
+        
+        if (currentCredit > 1)
+        {
+            TextMesh creditText = GameObject.Find("CreditText").GetComponent<TextMesh>();
+            int newCredit = currentCredit - stepSize;
+
+            currentCredit = newCredit;
+            creditText.text = newCredit.ToString();
+        }
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        UnityEngine.Debug.Log("Button released");
+    }
+}
---
+++ AUG-Project/Assets/Scripts/Custom/MegaCreditUp.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Vuforia;
+
+public class MegaCreditUp : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public int initialCredit = 20;
+    public int productionCredit = 1;
+    private int currentCredit = 20;
+    private int stepSize = 1;
+    private int frameCounter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject virtualButtonMegaCreditUp = GameObject.Find("MegaCreditUp");
+        virtualButtonMegaCreditUp.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        frameCounter++;
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        UnityEngine.Debug.Log("Button pressed");
+        UnityEngine.Debug.Log(currentCredit);
+
+        if (frameCounter > 5)
+        {
+            TextMesh creditText = GameObject.Find("MegaCreditText").GetComponent<TextMesh>();
+            int newCredit = currentCredit + stepSize;
+
+            currentCredit = newCredit;
+            creditText.text = newCredit.ToString();
+
+            frameCounter = 0;
+        }
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        UnityEngine.Debug.Log("Button released");
+
+        frameCounter = 0;
+    }
+}
